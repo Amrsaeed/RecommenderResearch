c6056389816245ba5267e3ca2cc9597d6c05f5d7
Amr Saeed <amrsaeed@aucegypt.edu>
2017-07-31 21:44:32 +0200

    Split training matrix into K nxm matrices

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from cluto.cluster import cluto_cluster
 from preprocess.movielens10m import preprocess as mvp
-
+import csv
+import numpy as np
 
 CLUTO_SCLUSTER_EXECUTABLE = "./cluto/scluster"
 CLUTO_VCLUSTER_EXECUTABLE = "./cluto/vcluster"
 MOVIELENS_DATA = './datasets/movielens/'
-# CLUSTERED_FILE = './sports.mat.clustering.10'
 
 NUMBER_USERS_MOVIELENS = 71567
 NUMBER_ITEMS_MOVIELENS = 10681
@@ -15,17 +15,31 @@ K = 10
 matrixFileName = mvp()
 CLUSTERED_FILE = cluto_cluster(K, matrixFileName)
 
-g = [0.5] * NUMBER_USERS_MOVIELENS
-g_complement = g
 clusters = [int(line.rstrip('\n')) for line in open(CLUSTERED_FILE)]
+rows_count = 0
+columns_count = 0
 
+with open("movielensCluto.mat", 'r') as f:
+    first_line = f.readline()
+    rows_count, columns_count, _ = first_line.split()
+
+R_training = np.zeros((K, int(rows_count), int(columns_count)))
+
+with open("R.csv", 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for i in range(int(rows_count)):
+        R_training[clusters[i]][i] = next(csvreader)
+
+g = [0.5] * int(rows_count)
+g_complement = g
 switched_users = len(g)
 switch_percent = 100
+
 print(switched_users)
 
 while switch_percent > 1:
     exit()
-    # Estimate S and Spu , ∀pu ∈ {1, . . . , k} with Equation 4.
+    # Estimate S and Spu , u  {1, . . . , k} with Equation 4.
     #   for all user u do
     #       for all cluster pu do
     #           Compute gu for cluster pu with Equation 5.
